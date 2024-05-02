9d0d5110756cafea6600851f5d5e52f78c7e1ba3
Miguel Arias <miguelariasgarau@gmail.com>
2024-05-02 11:11:36 +0200

    import components and discard non-useful importations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartService } from './services/cart.service';
+import { Cart } from './models/cart.model';
+import { StoreService } from './services/store.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    MatSidenavModule,
-    MatGridListModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatExpansionModule,
-    MatListModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatBadgeModule,
-    MatSnackBarModule,],
+    CommonModule, RouterOutlet, HeaderComponent, HomeComponent, HttpClientModule],
+  providers: [CartService, StoreService],
   template: `
-
+  <app-header [cart]="cart" />
+  <router-outlet />
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'store';
+export class AppComponent implements OnInit {
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart
+    })
+  }
+
+  cart: Cart = { items: [] }
 }
