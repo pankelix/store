6e412db7e1e01cb6b40fe7189413326674ec5279
Miguel Arias <miguelariasgarau@gmail.com>
2024-05-02 11:06:32 +0200

    add product box component functionalities

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Product } from '../../../../models/product.model';
+
+@Component({
+  selector: 'app-product-box',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatIconModule],
+  templateUrl: './product-box.component.html'
+})
+export class ProductBoxComponent {
+  @Input() fullWidthMode = false
+  @Input() product: Product | undefined
+  @Output() addToCart = new EventEmitter()
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product)
+  }
+}
