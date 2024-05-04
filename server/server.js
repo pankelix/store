8c969b925bc18d9c7bfc20c130ada301af657f27
Miguel Arias <miguelariasgarau@gmail.com>
2024-05-04 08:12:21 +0200

    add stripe paymenth option

--- server/server.js
+++ server/server.js
@@ -3,4 +3,81 @@ const cors = require('cors')
 const bodyparser = require('body-parser')
 
 const app = express()
-app
+app.use(express.static('public'))
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+app.use(cors({ origin: true, credentials: true }))
+
+const stripe = require('stripe')('sk_test_51PBvATRwmvtq6SYkkPoQ4mPgGAuAw0tJhXJIyo2uGFSBHfynUryXa5hdCBDMoG4T0tx97M2A43uYX3bgUngFcx4o00PX4l2Jdz')
+
+app.post('/checkout', async (req, res, next) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            shipping_address_collection: {
+                allowed_countries: ['ES'],
+            },
+            shipping_options: [
+                {
+                    shipping_rate_data: {
+                        type: 'fixed_amount',
+                        fixed_amount: {
+                            amount: 0,
+                            currency: 'eur',
+                        },
+                        display_name: 'Free shipping',
+                        delivery_estimate: {
+                            minimum: {
+                                unit: 'business_day',
+                                value: 5,
+                            },
+                            maximum: {
+                                unit: 'business_day',
+                                value: 7,
+                            },
+                        },
+                    },
+                },
+                {
+                    shipping_rate_data: {
+                        type: 'fixed_amount',
+                        fixed_amount: {
+                            amount: 1500,
+                            currency: 'eur',
+                        },
+                        display_name: 'Next day air',
+                        delivery_estimate: {
+                            minimum: {
+                                unit: 'business_day',
+                                value: 1,
+                            },
+                            maximum: {
+                                unit: 'business_day',
+                                value: 1,
+                            },
+                        },
+                    },
+                },
+            ],
+            line_items: req.body.items.map((item) => ({
+                price_data: {
+                    currency: 'eur',
+                    product_data: {
+                        name: item.name,
+                        images: [item.product]
+                    },
+                    unit_amount: item.price * 100
+                },
+                quantity: item.quantity,
+            })),
+            mode: 'payment',
+            success_url: 'http://localhost:4242/success.html',
+            cancel_url: 'http://localhost:4242/cancel.html'
+        })
+
+        res.status(200).json(session)
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.listen(4242, () => console.log('app is running on 4242'))
